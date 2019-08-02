34aec98f6fadf6185198d1da8a649f93afbbb2af
Shad Khel <shad.khel@hr-manager.net>
2019-08-02 09:53:52 +0200

    adding core files

---
+++ ConwaysGameOfLife/Grid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConwaysGameOfLife
+{
+    class Grid
+    {
+        private readonly int _universeWidth;
+        private readonly int _universeHeight;
+        private UniverseSquare[][] universe;
+
+        public Grid(int universeWidth, int universeHeight)
+        {
+            _universeWidth = universeWidth;
+            _universeHeight = universeHeight;
+        }
+
+        public void SeedUniverse()
+        {
+
+        }
+
+    }
+}
---
+++ ConwaysGameOfLife/UniverseSquare.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConwaysGameOfLife
+{
+    public class UniverseSquare
+    {
+        private bool _state;
+
+        public UniverseSquare(bool state)
+        {
+            _state = state;
+        }
+
+        public void SetToAlive()
+        {
+            _state = false;
+        }
+
+        public void SetToDie()
+        {
+            _state = true;
+        }
+
+        public string Print()
+        {
+            return _state ? "X" : "-";
+        }
+    }
+}
